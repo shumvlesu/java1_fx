d027d8ba5a272b3c764453617aa242697e556c74
sergey <sashumihin@gmail.com>
2020-12-22 23:23:13 +0300

    8 урок 2:06:14

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,41 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.util.Random;
+
 public class Controller {
+
+    @FXML
+    private TextField numberImputField;
+
+    @FXML
+    private Label randomNumberLabel;
+
+    //если надо выполнить какое-то событие при открытие окна то используем зарезервированое initialize()
+    @FXML
+    public void initialize() {
+
+    }
+
+    //наш обработчик события
+    @FXML
+    void doRandomAction() {
+        Random random = new Random();
+        String message = numberImputField.getText(); //получаю значение в виде текста из поля с id numberImputField
+        numberImputField.clear();
+
+        if (!message.isBlank()) {
+            try {
+                int number = Integer.parseInt(message); //преобразую из строки в тип int
+                int randomNumber = random.nextInt(number + 1);
+                randomNumberLabel.setText(String.valueOf(randomNumber));
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Привет мир!");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        //primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
